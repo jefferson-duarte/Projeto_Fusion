cfc7984234d6d64d5b55e9de0acc5bd3a7e9228d
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-17 23:30:06 +0100

    Configurado envio de email

--- core/views.py
+++ core/views.py
@@ -1,12 +1,26 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 from .models import Servico, Funcionario
+from .forms import ContatoForm
+from django.contrib import messages
 
-class IndexView(TemplateView):
+
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
     
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['servicos'] = Servico.objects.all().order_by('?')
         context['funcionarios'] = Funcionario.objects.all().order_by('?')
-        
         return context
+    
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso.')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+    
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar o e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
--- fusion/settings.py
+++ fusion/settings.py
@@ -132,3 +132,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
