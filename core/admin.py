bc45237f48d07baded131f0f92277562a4339123
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-17 13:55:13 +0100

    Configurado os models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
+from atexit import register
 from django.contrib import admin
+from .models import Servico, Funcionario, Cargo
 
-# Register your models here.
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = [
+        'servico', 'icone', 'ativo', 'modificado',
+    ]
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = [
+        'cargo', 'ativo', 'modificado',
+    ]
+    
+    
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome', 'cargo', 'ativo', 'modificado',    
+    ]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from stdimage.models import StdImageField
+import uuid
 
-# Create your models here.
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
+
+class Base(models.Model):
+    criado = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualizado', auto_now=True)
+    ativo = models.BooleanField('Ativo', default=True)
+    
+    class Meta:
+        abstract = True
+        
+    
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenamgem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    
+    servico = models.CharField('Serviços', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOICES)
+    
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+    
+    def __str__(self):
+        return self.servico
+
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+        
+    def __str__(self):
+        return self.cargo
+
+
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.CharField('Bio', max_length=200)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 400, 'height': 400, 'crop': True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')    
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+    
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+        
+    def __str__(self):
+        return self.nome
+    
