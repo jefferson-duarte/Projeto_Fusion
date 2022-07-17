dbcbc0a69290a3bf1b2ad4c0968dfc76af070804
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-17 22:49:00 +0100

    Adicionado o conteudo no servico e equipe

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
-
+from .models import Servico, Funcionario
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.all().order_by('?')
+        context['funcionarios'] = Funcionario.objects.all().order_by('?')
+        
+        return context
