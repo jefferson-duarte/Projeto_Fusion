3bb2535d258a8d6184123e89a34462bf79fc919e
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-18 08:48:03 +0100

    Fix import

--- fusion/settings.py
+++ fusion/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from utils import utils
+# from utils import utils
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
